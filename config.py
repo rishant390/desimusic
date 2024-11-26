1833fe3191409a2162547809a2575ac9b54cf188
rishant390 <hubbot4@gmail.com>
2024-11-26 19:21:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,8 +38,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/Hyper_Update")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Sr_Family_Grp")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ajisbackk")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/legendchatting57")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -77,10 +77,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org/file/872c91ddaffa149e9767e.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/xk9o83.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://graph.org/file/acda3630fd3f07feae742.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/xk9o83.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://graph.org/file/2fd73ef39c62047594009.jpg"
